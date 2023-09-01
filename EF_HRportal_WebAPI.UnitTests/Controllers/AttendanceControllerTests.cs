0677816dbd4a77bcb0ad826bf90e25f280733b0b
Banuyaswanth <mallinenibanu@gmail.com>
2023-09-01 17:34:12 +0530

    Added Unit Tests project to the solution

---
+++ EF_HRportal_WebAPI.UnitTests/Controllers/AttendanceControllerTests.cs
@@ -0,0 +1,17 @@
+using Xunit;
+using AutoFixture;
+using Moq;
+using FluentAssertions;
+
+namespace EF_HRportal_WebAPI.UnitTests.Controllers
+{
+    public class AttendanceControllerTests
+    {
+
+        [Fact]
+        public void Test1()
+        {
+            //unit test
+        }
+    }
+}
